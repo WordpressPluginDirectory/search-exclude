3b82c1f31d0c599eaf166d023f6f3655a089a25a
Justin Gardner <justin@rhynorater.com>
2025-01-22 06:04:37 -0500

    2.4.0

--- search-exclude/build/settings/js/index.asset.php
+++ search-exclude/build/settings/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('qlse-store', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '05f87870f90bea145f57');
+<?php return array('dependencies' => array('qlse-store', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '4fb0635b6e0b37482390');
